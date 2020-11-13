84aa5dd1ddab1632d8ec1ba74cc59f89a2c89efc
Rom Grk <romgrk.cc@gmail.com>
2020-11-13 10:37:31 -0500

    fix: check autocmds for all windows in all tabs

--- src/nvim/api/window.c
+++ src/nvim/api/window.c
@@ -192,7 +192,7 @@ void nvim_win_set_height(Window window, Integer height, Error *err)
   curwin = savewin;
   try_end(err);
 
-  FOR_ALL_WINDOWS_IN_TAB(wp, curtab) {
+  FOR_ALL_TAB_WINDOWS(tp, wp) {
     autocmd_check_window_scrolled(wp);
   }
 }
@@ -241,7 +241,7 @@ void nvim_win_set_width(Window window, Integer width, Error *err)
   curwin = savewin;
   try_end(err);
 
-  FOR_ALL_WINDOWS_IN_TAB(wp, curtab) {
+  FOR_ALL_TAB_WINDOWS(tp, wp) {
     autocmd_check_window_scrolled(wp);
   }
 }
