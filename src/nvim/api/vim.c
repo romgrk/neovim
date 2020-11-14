f07c97de25174087f2949793acd88158e56f4515
Rom Grk <romgrk.cc@gmail.com>
2020-11-14 06:08:54 -0500

    fix: remove unuseful check

--- src/nvim/api/vim.c
+++ src/nvim/api/vim.c
@@ -974,12 +974,6 @@ void nvim_set_option(uint64_t channel_id, String name, Object value, Error *err)
   FUNC_API_SINCE(1)
 {
   set_option_to(channel_id, NULL, SREQ_GLOBAL, name, value, err);
-
-  if (strequal(name.data, "wrap")) {
-    FOR_ALL_WINDOWS(wp) {
-      autocmd_check_window_scrolled(wp);
-    }
-  }
 }
 
 /// Writes a message to the Vim output buffer. Does not append "\n", the
